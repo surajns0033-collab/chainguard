3a6eabeead636272483c5fd92cf857f33c504ad8
Suraj <suraj.n.s0033@gmail.com>
2026-09-01 02:39:51 +0530

    ChainGuard

--- frontend/vertex-ai-proxy-interceptor.js
+++ frontend/vertex-ai-proxy-interceptor.js
@@ -113,7 +113,7 @@
           headers: {
             'Content-Type': 'application/json',
             // Add a random header to identify these proxied requests on the Node.js backend.
-            'X-App-Proxy': '7xZ8VdUm3vXqaOmBMYi3e0ROSHm5fq-6',
+            'X-App-Proxy': 'vkwOW1rffdTATJyau6fIwq6Ygw9_wJoN',
           },
           body: JSON.stringify(requestDetails),
         };
